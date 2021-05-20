0f39e6766e720c6cfdeae2e23439ad832c930550
45gfg9 <mc45@qq.com>
2021-05-21 00:31:04 +0800

    RF24 stub, compile pass

--- src/rf24.cpp
+++ src/rf24.cpp
@@ -1,26 +1,38 @@
 #include <RemoteSignal.hxx>
 #include <RF24.h>
 
-void begin() {
+void rf24::begin() {
   DDRC = _BV(PC0) | _BV(PC1);
 
   spi::begin();
 }
 
-void end() {
+void rf24::end() {
   spi::end();
 
   DDRC = 0;
 }
 
-uint8_t rx() {
+bool rf24::ready() {
+  // TODO
+  return false;
+}
+
+bool rf24::available() {
+  // Stub
+  return bit_is_clear(PIND, PD2);
+}
+
+uint8_t rf24::rx() {
   using spi::rx;
 
   return rx();
 }
 
-bool tx(uint8_t data) {
+bool rf24::tx(uint8_t data) {
   using spi::tx;
 
   tx(data);
+
+  return true;
 }
