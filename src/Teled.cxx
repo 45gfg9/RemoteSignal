03df2ecefb8e4e53c7ab6c6bf49343b4c8b22b65
45gfg9 <mc45@qq.com>
2021-06-19 21:26:48 +0800

    Init RF24

--- src/Teled.cxx
+++ src/Teled.cxx
@@ -1,14 +1,16 @@
 #include <Teled.hxx>
 
 int main() {
-  power_all_disable();
-
   timer2::init();
+
+  power_all_disable();
 
   io::init();
 
   spi::init();
   wdt::init();
+
+  rf24::init();
 
   set_sleep_mode(SLEEP_MODE_PWR_DOWN);
   sleep_enable();
